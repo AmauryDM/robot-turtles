738107ae84a2acd67ff8ea42df3d66b6424e1222
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-11 00:30:47 +0100

    Update Tour.java

--- Robot Turtle/src/com/company/Tour.java
+++ Robot Turtle/src/com/company/Tour.java
@@ -1,16 +1,49 @@
 package com.company;
 
-import static com.company.Complete.completeProgram;
-import static com.company.Execution.executeProgram;
-import static com.company.Wall.buildWall;
-
 public class Tour {
 
-    public static void Play() {
+  public void end() {
+      Scanner scanner = new Scanner(System.in);
+      boolean run = true;
+      int input;
+      int numberDiscard = 0;
 
-        executeProgram();
-        buildWall();
-        completeProgram();
-    }
+      System.out.println("FIN DU TOUR");
+      this.getDeck();
+      System.out.println("1. Se débarrasser de certaines cartes");
+      System.out.println("2. Terminer son tour");
 
+      input = scanner.nextInt();
+
+      if (input == 1) {
+          do {
+              System.out.println("De quelle carte se défausser ?");
+              System.out.println("(0 pour terminer la saisie)");
+              input = scanner.nextInt();
+
+              if (input < 0 || input > main.size()) {
+                  System.out.println("Erreur dans la saisie");
+              }
+
+              else if (saisie == 0) {
+                  run = false;
+              }
+
+              else {
+                  Card chosen = main.get(saisie - 1);
+                  playerDeck.remove(chosen);
+                  numberDiscard += 1;
+                  this.getDeck();
+              }
+          } while (run);
+
+          while (playerDeck.size() < 5) {
+              playerDeck.add(deck.get(0));
+              deck.remove(0);
+          }
+      }
+      else (input == 2) {
+        number += 1;
+      }
+  }
 }
