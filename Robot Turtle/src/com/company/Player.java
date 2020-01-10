a650d255a18c45e6564d99a5e5576b9a093e1165
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-10 22:41:40 +0100

    Update Player.java

--- Robot Turtle/src/com/company/Player.java
+++ Robot Turtle/src/com/company/Player.java
@@ -4,22 +4,77 @@ import java.util.*;
 
 public class Player extends Turtle{
 
-    public int number;
-    private String color;
-    private String direction;
-    private int position;
+  public int number;
+  private String color;
+  private String direction;
+  private int position;
 
-    private ArrayList<Card> deck;
-    private ArrayList<Card> discard;
+  public ArrayList<Card> playerDeck = new ArrayList<Card>;
+  public ArrayList<Wall> playerWalls = new ArrayList<Wall>;
+  public ArrayList<Card> discard = new ArrayList<Card>;
 
-    public void setDeck(ArrayList<Card> deck) {
-        this.deck = deck;
+  public ArrayList<Card> program = new ArrayList<Card>;
+
+  public Player(String name, String color) {
+    this.name = name;
+    this.color = color;
+    this.direction = "N";
+
+    Deck initializeDeck = new Deck();
+    this.deck = initializeDeck.createDeck();
+    this.walls = initializeDeck.createWalls();
+
+    for (int i = 0; i < 5; i++) {
+      playerDeck.add(deck.get(0));
+      deck.remove(0);
+    }
+  }
+
+  public void displayProgram() {
+    if (!program.isEmpty()) {
+      for (Card card : program) {
+        System.out.println(card.getName());
+      }
+    }
+    else {
+      System.out.println("Le programme est vide");
+    }
+  }
+
+  public void turn(String direction) {
+    if(direction = "N") {
+      direction = "E";
+    }
+    else if(direction = "E") {
+      direction = "S";
+    }
+    else if(direction = "S") {
+      direction = "W";
+    }
+    else if(direction = "W") {
+      direction = "N";
+    }
+  }
+
+  public Case destination(Tray tray) {
+    Case destination = new Case(0, 0);
+
+    if (direction == "N") {
+      caseVisee = tray.positionCase(x+1, y);
     }
 
-    public void setDiscard(ArrayList<Card> discard) {
-        this.discard = discard;
+    if (direction == "S") {
+        destination = tray.positionCase(x, y-1);
     }
 
-    
+    if (direction == "E") {
+        destination = tray.positionCase(x, y+1);
+    }
 
+    if (direction == "W") {
+        destination = tray.positionCase(x-1, y);
+    }
+
+    return destination;
+  }
 }
