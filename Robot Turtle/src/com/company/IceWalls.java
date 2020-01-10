1623cf8100ab39d7947ed5978f63274a3b60c29e
Juliette Bégué <juliette.b92@gmail.com>
2020-01-10 18:20:08 +0100

    Create IceWalls.java

---
+++ Robot Turtle/src/com/company/IceWalls.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class IceWalls extends Walls {
+
+    public String getCharacteristic() {
+        return this.characteristic;
+    }
+
+    protected boolean isDestroyable;
+
+    public boolean getIsDestroyable() {
+        return this.isDestroyable;
+    }
+
+    public String getName() {
+        return this.name="";
+    }
+
+    public String getType() {
+        return this.type="Mur";
+    }
+}
+
