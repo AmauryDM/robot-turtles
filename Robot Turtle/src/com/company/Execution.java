5c50315b97e79f5e5b0f6a11faaba5d3eda2aae5
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-11 00:30:39 +0100

    Update Execution.java

--- Robot Turtle/src/com/company/Execution.java
+++ Robot Turtle/src/com/company/Execution.java
@@ -2,8 +2,15 @@ package com.company;
 
 public class Execution extends Tour {
 
-    public static void executeProgram() {
-
+    public static void executeProgram(Tray tray, Player player) {
+      if (!program.isEmpty()) {
+        for (Card card : program) {
+          card.actionCard(tray, player);
+        }
+        program.clear();
+      }
+      else {
+        System.out.println("Le programme est vide");
+      }
     }
-
 }
