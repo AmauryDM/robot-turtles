c247b6f8ddf5e231d63dc2c09df2fcb98ca57a9d
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-10 22:41:31 +0100

    Update Complete.java

--- Robot Turtle/src/com/company/Complete.java
+++ Robot Turtle/src/com/company/Complete.java
@@ -1,9 +1,35 @@
 package com.company;
 
+import java.util.*;
+
 public class Complete extends Tour {
 
-    public static void completeProgram() {
+    public void completeProgram() {
+      Scanner scanner = new Scanner(System.in);
+      int input = new int;
+      boolean continue = true;
 
-    }
+        System.out.println("Voici votre deck :");
+        this.getDeck();
+        do {
+          System.out.println("Quelle carte voulez-vous ajouter au programme ?");
+          System.out.println("Entrez 0 pour arrêter la saisie");
+          inputNumber = scanner.nextInt();
 
+          if (inputNumber < 0 || inputNumber > deck.size()) {
+              System.out.println("Erreur dans la saisie");
+          }
+
+          else if (inputNumber == 0) {
+              continue = false;
+          }
+
+          else {
+            Card choosen = playerDeck.get(input - 1);
+            this.program.add(choosen);
+            this.playerDeck.remove(choosen);
+            this.getDeck();
+          }
+        } while(continue);
+  }
 }
