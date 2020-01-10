65dede8cbe0372be1347b64babd5d84bd8e5a102
Juliette Bégué <juliette.b92@gmail.com>
2020-01-10 20:26:47 +0100

    Update Jewel.java

--- Robot Turtle/src/com/company/Jewel.java
+++ Robot Turtle/src/com/company/Jewel.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class Jewel extends Turtle {
+    private String color;
 
     String direction;
     int position;
@@ -9,5 +10,9 @@ public class Jewel extends Turtle {
         Jewel jewel = new Jewel();
         System.out.println(jewel.direction);
         System.out.println(jewel.position);
+    }
+
+    public String ShowColor() {
+        return this.color;
     }
 }
