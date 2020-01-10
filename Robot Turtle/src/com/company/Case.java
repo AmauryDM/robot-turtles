aaf67a16f9677cb9f7da980829f3d70eff1a3fce
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-10 19:28:39 +0100

    Create Case.java

---
+++ Robot Turtle/src/com/company/Case.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Case {
+
+  private int xCoordinate;
+  private int yCoordinate;
+  private Turtle content;
+
+  public Case(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int[] getPosition(){
+    int[] position = {this.x, this.y};
+    return position;
+  }
+
+  public Turtle contentEmpty() {
+    if(content == null){
+      Turtle turtle = new Turtle();
+      turtle.getType(" ");
+      return turtle
+    }
+    return content;
+  }
+
+  public boolean emptyCase() {
+        if (contenu == null) {
+          return true;
+        }
+        else {
+            System.out.println("La case (" + this.getPosition()[0] + "," + this.getPosition()[1] + ") n'est pas vide !");
+            return false;
+        }
+    }
+}
