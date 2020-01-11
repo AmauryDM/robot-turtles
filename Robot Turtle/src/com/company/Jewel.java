4b9b5c56f390eaed7ce7cbbdeee3a66245e48ce5
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-11 01:00:16 +0100

    Update Jewel.java

--- Robot Turtle/src/com/company/Jewel.java
+++ Robot Turtle/src/com/company/Jewel.java
@@ -3,6 +3,7 @@ package com.company;
 public class Jewel extends Turtle {
 
   private String name;
+  private Turtle turtle;
 
   public Jewel () {
     this.name = name;
@@ -13,5 +14,25 @@ public class Jewel extends Turtle {
         Jewel jewel = new Jewel();
         System.out.println(jewel.direction);
         System.out.println(jewel.position);
+    }
+
+    public void winPlayer() {
+      if (number == 2) {
+        if (turtle.getCoordinate(0) == 7 && turtle.getCoordinate(1) == 3) {
+          System.out.println("Vous avez gagné");
+          turtle.empty();
+        }
+      }
+      else if (number == 3) {
+        if ((turtle.getCoordinate(0) == 7 && turtle.getCoordinate(1) == 0) || (turtle.getCoordinate(0) == 7 && turtle.getCoordinate(1) == 3) || (turtle.getCoordinate(0) == 7 && turtle.getCoordinate(1) == 6))
+        System.out.println("Vous avez gagné");
+        turtle.empty();
+      }
+      else if (number == 4) {
+        if ((turtle.getCoordinate(0) == 7 && turtle.getCoordinate(1) == 1) || (turtle.getCoordinate(0) == 7 && turtle.getCoordinate(1) == 6)) {
+          System.out.println("Vous avez gagné");
+          turtle.empty();
+        }
+      }
     }
 }
