a2dbf42f2ed23fe2c3738e7b040d990bb210a974
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-11 01:50:58 +0100

    Create YellowCard.java

---
+++ Robot Turtle/src/com/company/YellowCard.java
@@ -0,0 +1,9 @@
+package com.company;
+*
+public class YellowCard extends Card {
+
+  public YellowCard() {
+    this.type = "Jaune"
+    this.description = "Rotation d'un quart de tour dans le sens anti-horaire";
+  }
+}
