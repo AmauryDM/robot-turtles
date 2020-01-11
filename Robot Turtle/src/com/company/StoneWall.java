78ce7f4f38d9285224679fdb8f672dda4c4ff73e
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-11 01:18:03 +0100

    Update StoneWall.java

--- Robot Turtle/src/com/company/StoneWall.java
+++ Robot Turtle/src/com/company/StoneWall.java
@@ -2,21 +2,19 @@ package com.company;
 
 public class StoneWall extends Wall {
 
-    public String getCharacteristic() {
-        return this.characteristic;
-    }
+  public String getCharacteristic() {
+      return this.characteristic = "Mur indestructible";
+  }
 
-    protected boolean isDestroyable;
+  public boolean getIsDestroyable() {
+      return this.isDestroyable;
+  }
 
-    public boolean getIsDestroyable() {
-        return this.isDestroyable;
-    }
+  public String getName() {
+      return this.name = "pierre";
+  }
 
-    public String getName() {
-        return this.name;
-    }
-
-    public String getType() {
-        return this.type="Mur";
-    }
+  public String getType() {
+      return this.type = "Mur";
+  }
 }
