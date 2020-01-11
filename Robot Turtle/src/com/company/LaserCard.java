763158a22d2e156fccc7d9fe07400f690c8c4f55
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-11 01:50:52 +0100

    Create LaserCard.java

---
+++ Robot Turtle/src/com/company/LaserCard.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class LaserCard extends Card {
+
+  public LaserCard() {
+    this.type = "Laser"
+    this.description = "Détruit les objets devant la tortue";
+  }
+}
