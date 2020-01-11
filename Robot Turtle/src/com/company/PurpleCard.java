aa40bbe9865c1be5b3fd391245c9043cc727a8ae
AmauryDM <44876898+AmauryDM@users.noreply.github.com>
2020-01-11 01:50:55 +0100

    Create PurpleCard.java

---
+++ Robot Turtle/src/com/company/PurpleCard.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class PurpleCard extends Card {
+
+  public PurpleCard() {
+    this.type = "Violet"
+    this.description = "Rotation d'un quart de tour dans le sens anti-horaire";
+  }
+}
